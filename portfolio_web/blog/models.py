b3f577d34bc0271c0e2b979925453c361893bd32
cherab19 <cheramlak21@gmail.com>
2025-10-17 21:45:52 +0300

    feat: define core data models for projects, blog, and contact

--- portfolio_web/blog/models.py
+++ portfolio_web/blog/models.py
@@ -1,3 +1,22 @@
+# blog/models.py
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    excerpt = models.TextField(max_length=300, help_text="Brief summary of the post")
+    featured_image = models.ImageField(upload_to='blog/', blank=True)
+    published_date = models.DateTimeField(null=True, blank=True)
+    is_published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if self.is_published and not self.published_date:
+            from django.utils import timezone
+            self.published_date = timezone.now()
+        super().save(*args, **kwargs)
