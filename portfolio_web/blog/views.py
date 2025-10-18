05a846d8af6764cc799569b072bbb2117f610fa6
Chernet Degefe <cheramlak21@gmail.com>
2025-10-18 11:14:52 +0300

    feat: implement admin panel for data management

--- portfolio_web/blog/views.py
+++ portfolio_web/blog/views.py
@@ -1,8 +1,11 @@
 # blog/views.py
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import BlogPost
 
 def blog_list(request):
-    return render(request, 'blog/blog_list.html')
+    posts = BlogPost.objects.filter(is_published=True).order_by('-published_date')
+    return render(request, 'blog/blog_list.html', {'posts': posts})
 
 def blog_detail(request, slug):
-    return render(request, 'blog/blog_detail.html')
+    post = get_object_or_404(BlogPost, slug=slug, is_published=True)
+    return render(request, 'blog/blog_detail.html', {'post': post})
