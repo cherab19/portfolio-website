39136ad19fef8a81434a93d72a03225f6c7d9ba5
Chernet Degefe <cheramlak21@gmail.com>
2025-10-20 21:26:07 +0300

    feature applied on core app

--- core/models.py
+++ core/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+class SkillCategory(models.Model):
+    name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
 class Skill(models.Model):
 	name = models.CharField(max_length=200)
